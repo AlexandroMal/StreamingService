d724acd05057566aa5b27d2940f50de1776706c5
AlexandroMal <Sashamalina8695@gmail.com>
2023-09-22 18:52:14 +0300

    make context for add database

---
+++ StreamingService.DAL/Context/StreamingDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StreamingService.DAL.Entities;
+using StreamingService.DAL.EntityConfiguration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamingService.DAL.Context
+{
+    public class StreamingDbContext : DbContext
+    {
+        public StreamingDbContext(DbContextOptions options) 
+            : base(options) { }
+
+        public DbSet<Video> Videos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new VideoConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
