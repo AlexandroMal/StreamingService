743a43b14aa47e0fa01ae223d156d126722fe5d1
AlexandroMal <Sashamalina8695@gmail.com>
2023-09-22 18:43:37 +0300

    create api for StreamigService and connect database in project

---
+++ StreamingServiceApi/Program.cs
@@ -0,0 +1,29 @@
+using StreamingService.DAL.Entities;
+using StreamingService.DAL.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+IConfiguration configuration = builder.Configuration;
+
+builder.Services.InjectDatabaseServices(configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
