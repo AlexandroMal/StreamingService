8e80401bab6181f3a2d9f816b8a545bd67bc3ea0
AlexandroMal <Sashamalina8695@gmail.com>
2023-09-22 18:46:09 +0300

    create configuration for video entity

---
+++ StreamingService.DAL/EntityConfiguration/VideoConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StreamingService.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamingService.DAL.EntityConfiguration
+{
+    public class VideoConfiguration : IEntityTypeConfiguration<Video>
+    {
+        public void Configure(EntityTypeBuilder<Video> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.UploadVideo).IsRequired();
+            builder.Property(x => x.FilePath).IsRequired();
+            builder.Property(x => x.VideoSize);
+
+        }
+    }
+}
