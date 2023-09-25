bac80bed58bfad93d9a1aca30e2265503e104c74
AlexandroMal <Sashamalina8695@gmail.com>
2023-09-25 18:16:29 +0300

    add user and stream entity

---
+++ StreamingService.DAL/Entities/StreamPost.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamingService.DAL.Entities
+{
+    public class StreamPost : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ConnectInfo { get; set; }
+        public bool StreamStatus { get; set; }
+
+        public Guid CreatedForUser { get; set; }
+
+        public virtual User? User { get; set; }
+        public virtual ICollection<Video>? Videos { get; set; }
+
+
+    }
+}
