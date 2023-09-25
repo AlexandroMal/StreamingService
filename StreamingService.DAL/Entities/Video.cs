1acb4182eae2cc162731a9c881252cf0cd80c99f
AlexandroMal <Sashamalina8695@gmail.com>
2023-09-25 18:15:59 +0300

    change Video entity

--- StreamingService.DAL/Entities/Video.cs
+++ StreamingService.DAL/Entities/Video.cs
@@ -8,10 +8,13 @@ namespace StreamingService.DAL.Entities
 {
     public class Video : BaseEntity
     {
-        public string Name { get; set; }
-        public string VideoSize { get; set; }     
+        public string Name { get; set; }    
         public string FilePath { get; set; }
         public DateTime UploadVideo { get; set; }
+
+        public Guid CreatedForStreamPost { get; set; }
+
+        public virtual StreamPost? StreamPost { get; set; }
 
     }
 }
