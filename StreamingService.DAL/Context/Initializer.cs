6c96da7be39bbd1eeee7f37ed6fa35f6143c04d7
AlexandroMal <Sashamalina8695@gmail.com>
2023-09-25 18:14:34 +0300

    add Initializer in project

---
+++ StreamingService.DAL/Context/Initializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamingService.DAL.Context
+{
+    public class Initializer
+    {
+        public static void Initialize(StreamingDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
